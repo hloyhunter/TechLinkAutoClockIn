ddb615cacb256ee3cfedb66b050932f9d59517f4
聞冠宥 <hloyhunter@hotmail.com>
2017-07-27 22:14:18 +0800

    第一完成版，準備封裝

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,17 +1,5 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-var settings = new Store("settings", {
-    "sample_setting": "This is how you use Store.js to remember values"
-});
-
-
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
-
-chrome.browserAction.onClicked.addListener(function(tab) { 
-	alert('icon clicked');
+chrome.browserAction.onClicked.addListener(function(activeTab)
+{
+    var newURL = "https://femascloud.com/techlink";
+    chrome.tabs.create({ url: newURL });
 });
